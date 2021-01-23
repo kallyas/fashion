30f5280bbeb47dda9d6197d0ea42fc5e98fa4277
Kally <kallyasmedia@gmail.com>
2021-01-23 10:43:12 +0000

    add new config

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import OneSignal from "react-native-onesignal";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { APP_ID } from "src/src/config/onesignal";
+import "src/src/config-i18n";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppRouter from "src/AppRouter";
+
+import NavigationService from "src/utils/navigation";
+
+import configureStore from "src/config-store";
+import { getDemoSelector } from "src/modules/common/selectors";
+import { tokenSelector } from "src/modules/auth/selectors";
+import demoConfig from "src/utils/demo";
+import globalConfig from "src/utils/global";
+
+const { store, persistor } = configureStore();
+
+// type Props = {};
+
+class App extends Component {
+  componentDidMount() {
+    OneSignal.init(APP_ID);
+
+    OneSignal.addEventListener("received", this.onReceived);
+    OneSignal.addEventListener("opened", this.onOpened);
+    OneSignal.addEventListener("ids", this.onIds);
+
+    store.subscribe(() => {
+      const state = store.getState();
+      demoConfig.setData(getDemoSelector(state).toJS());
+      globalConfig.setToken(tokenSelector(state));
+    });
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener("received", this.onReceived);
+    OneSignal.removeEventListener("opened", this.onOpened);
+    OneSignal.removeEventListener("ids", this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log("Message: ", openResult.notification.payload.body);
+    console.log("Data: ", openResult.notification.payload.additionalData);
+    console.log("isActive: ", openResult.notification.isAppInFocus);
+    console.log("openResult: ", openResult);
+  }
+
+  onIds(device) {
+    console.log("Device info: ", device);
+  }
+
+  render() {
+    return (
+      <NavigationContainer
+        ref={(navigationRef) =>
+          NavigationService.setTopLevelNavigator(navigationRef)
+        }
+      >
+        <SafeAreaProvider>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <AppRouter />
+            </PersistGate>
+          </Provider>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
