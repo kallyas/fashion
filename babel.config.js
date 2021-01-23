759ec437e4885ee7bd1175833db9b4f468b58db0
Kally <kallyasmedia@gmail.com>
2021-01-23 11:38:56 +0000

    install updates

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,32 @@
+const path = require("path");
+
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            src: path.resolve(__dirname, "src/"),
+          },
+        },
+      ],
+    ],
   };
 };
 
-const path = require("path");
-
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        alias: {
-          src: path.resolve(__dirname, "src/"),
-        },
-      },
-    ],
-  ],
-};
+// module.exports = {
+//   presets: ["module:metro-react-native-babel-preset"],
+//   plugins: [
+//     [
+//       "module-resolver",
+//       {
+//         alias: {
+//           src: path.resolve(__dirname, "src/"),
+//         },
+//       },
+//     ],
+//   ],
+// };
