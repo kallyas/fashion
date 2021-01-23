9e68b6e398158fbcbff48e9161b19e1252e51f4d
Kally <kallyasmedia@gmail.com>
2021-01-23 10:29:48 +0000

    add config files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
+};
+
+const path = require("path");
+
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          src: path.resolve(__dirname, "src/"),
+        },
+      },
+    ],
+  ],
 };
